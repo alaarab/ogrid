ed6e8795ed01f13ae207c41b93af71611e61c4d2
Ala Arab <alaarab@gmail.com>
2026-02-13 12:07:43 -0800

    extract header filter base class

--- packages/angular-primeng/src/index.ts
+++ packages/angular-primeng/src/index.ts
@@ -7,7 +7,7 @@ export { DataGridTableComponent } from './datagrid-table/datagrid-table.componen
 export { InlineCellEditorComponent } from './datagrid-table/inline-cell-editor.component';
 export { PopoverCellEditorComponent } from './datagrid-table/popover-cell-editor.component';
 export { ColumnHeaderFilterComponent } from './column-header-filter/column-header-filter.component';
-export type { IColumnHeaderFilterProps } from './column-header-filter/column-header-filter.component';
+// IColumnHeaderFilterProps is now exported from @alaarab/ogrid-angular (base class)
 export { ColumnChooserComponent } from './column-chooser/column-chooser.component';
 export type { IColumnChooserProps } from './column-chooser/column-chooser.component';
 export { PaginationControlsComponent } from './pagination-controls/pagination-controls.component';
