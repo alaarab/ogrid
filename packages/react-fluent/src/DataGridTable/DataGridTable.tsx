ebac7c7522a306ebf40b8424dc694c9a34ddd854
Ala Arab <alaarab@gmail.com>
2026-02-27 12:38:42 -0800

    fix header menu toggle

--- packages/react-fluent/src/DataGridTable/DataGridTable.tsx
+++ packages/react-fluent/src/DataGridTable/DataGridTable.tsx
@@ -501,7 +501,11 @@ function DataGridTableInner<T>(props: IOGridDataGridProps<T>): React.ReactElemen
                                 className={styles.headerMenuTrigger}
                                 onClick={(e) => {
                                   e.stopPropagation();
-                                  headerMenu.open(col.columnId, e.currentTarget);
+                                  if (headerMenu.isOpen && headerMenu.openForColumn === col.columnId) {
+                                    headerMenu.close();
+                                  } else {
+                                    headerMenu.open(col.columnId, e.currentTarget);
+                                  }
                                 }}
                                 aria-label="Column options"
                                 title="Column options"
